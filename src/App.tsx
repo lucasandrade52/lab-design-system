fdaa8ac442f6581dca33294cfa65d17e876a009e
Lucas Nogueira <lucasandrade_52@hotmail.com>
2022-10-13 19:08:45 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import "./styles/global.css";
+
+function App() {
+  return <h1 className="font-bold text-gray-400">Hello</h1>;
+}
+
+export default App;
---
+++ storybook-static/assets/preview.5e9023cf.js
@@ -0,0 +1,2 @@
+import{t as o}from"./index.ee46e990.js";import"./iframe.167d47e3.js";import"./es.map.constructor.92a6ae4b.js";import"./es.number.to-fixed.d47662b8.js";import"./index.94138a2c.js";const s={actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}},docs:{theme:o.dark}};export{s as parameters};
+//# sourceMappingURL=preview.5e9023cf.js.map
