be13c69e31f0ee49d2397c94265653e8555fdd15
Lucas Nogueira <lucasandrade_52@hotmail.com>
2022-10-14 16:07:59 -0300

    finish design page

--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -7,7 +7,7 @@ export interface TextInputRootProps {
 
 function TextInputRoot(props: TextInputRootProps) {
   return (
-    <div className="flex items-center gap-3 h-12  py-4 px-3 rounded bg-gray-800 w-full focus-within:ring-2 ring-cyan-500">
+    <div className="flex items-center gap-3 h-12 py-4 px-3 rounded bg-gray-800 w-full focus-within:ring-2 ring-cyan-500">
       {props.children}
     </div>
   );
